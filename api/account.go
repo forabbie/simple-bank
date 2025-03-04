9dfa568be7b6c19f50f689254ed1c54975bad2bb
abbie <supermercadoabbie@gmail.com>
2025-03-04 15:26:05 +0800

    Added Users Database query and test + handling of DB errors

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/forabbie/vank-app/database/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -27,6 +28,16 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusConflict, errorResponse(err))
+				return
+			case "foreign_key_violation":
+				ctx.JSON(http.StatusNotFound, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
